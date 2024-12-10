2797b9739071f9ce9f30bdee6eca18363a708691
Rapina <rapina.sv04@gmail.com>
2024-12-09 22:26:36 -0300

    Teste

--- assets/main.js
+++ assets/main.js
@@ -2,27 +2,32 @@ const header = document.getElementById("header");
 const titulo = document.getElementById("title");
 const nav = document.getElementById("nav");
 
-setInterval(() => {
+window.addEventListener("scroll", () => {
     let scroll = window.scrollY * 10;
-
     // header animation
     if (scroll < 5000) {
         header.style.backgroundColor = `rgba(27, 34, 46, ${1 - scroll / 10000 })`;
-        header.style.height = `${25 - scroll / 500}vh`
+        header.style.height = `${20 - scroll / 500}vh`
         titulo.style.letterSpacing = `${2 - scroll / 3000}vw`;
         titulo.style.fontSize = `${3 - scroll / 10000}vw`;
         titulo.style.fontWeight = `${300 - scroll / 50}`;
         titulo.style.transform = `translateX(${0 - scroll / 150}vw)`;
-        nav.style.opacity = `${scroll / 5000}`;
-        nav.style.transform = `translateX(${0 + scroll / 150}vw)`;
+        if (scroll > 2500) {
+            nav.style.display = "block";
+        } else {
+            nav.style.display = "none"
+        }
+        nav.style.opacity = `${-1 + (scroll / 2500)}`;
+        nav.style.transform = `translateX(${0 + scroll / 200}vw)`;
     } else {
         header.style.backgroundColor = "rgba(27, 34, 46, 0.5)";
-        header.style.height = "15vh";
+        header.style.height = "10vh";
         titulo.style.letterSpacing = "0.34vw";
         titulo.style.fontSize = "2.5vw";
         titulo.style.fontWeight = "200";
         titulo.style.transform = "translateX(-33.33vw)";
         nav.style.opacity = "1";
-        nav.style.transform = "translateX(33.33vw)";
+        nav.style.transform = "translateX(25vw)";
+        nav.style.display = "block";
     }
-}, 10);
+});
